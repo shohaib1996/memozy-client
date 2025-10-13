4f9c7d92dd48bf86dd67989514595f9dfc2d3681
shohaib1996 <khanshohaibhossain@gmail.com>
2025-10-13 17:32:29 +0600

    review section added

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "*", // allow all hostnames
+        port: "",
+        pathname: "**", // allow all paths
       },
     ],
   },
